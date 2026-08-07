07390aaa6219d2ff0d76518adce7b05b8bf1c1a7
Lorifel <lorifel@mail.ru>
2026-08-07 06:46:17 +0300

    Stable 1.5.0 - fixed loading starting pose for animations

--- FurnitureAnimations/Plugin.cs
+++ FurnitureAnimations/Plugin.cs
@@ -14,7 +14,7 @@ namespace FurnitureAnimationsMod
     {
         public const string PluginGuid = "com.lorifel007.furnitureanimations";
         public const string PluginName = "Furniture Animations Mod";
-        public const string PluginVersion = "1.4.1";
+        public const string PluginVersion = "1.5.0";
 
         public static ManualLogSource Log;
         private Harmony harmony;
