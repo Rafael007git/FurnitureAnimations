064508281b22608c31ee4eec9402a7f47e3c544c
Lorifel <lorifel@mail.ru>
2026-08-14 02:08:06 +0300

    1.6.4 (not very stable)

--- FurnitureAnimations/Plugin.cs
+++ FurnitureAnimations/Plugin.cs
@@ -14,7 +14,7 @@ namespace FurnitureAnimationsMod
     {
         public const string PluginGuid = "com.lorifel007.furnitureanimations";
         public const string PluginName = "Furniture Animations Mod";
-        public const string PluginVersion = "1.6.3";
+        public const string PluginVersion = "1.6.4";
 
         public static ManualLogSource Log;
         private Harmony harmony;
